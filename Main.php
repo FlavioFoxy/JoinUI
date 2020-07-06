2ba11741eb8b31c68c4f5b3cec87010c7e26c56f
FlavioFoxy <67879916+FlavioFoxy@users.noreply.github.com>
2020-07-06 17:23:21 +0200

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,58 +1,60 @@
 <?php
 
-namespace FoxyUI;
+namespace flaviofoxy\joinUI;
 
 use pocketmine\plugin\PluginBase;
 
-use pocketmine\Player;
-use pocketmine\Server;
-
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\PlayerJoinEvent;
+
+use pocketmine\Server;
+use pocketmine\Player;
 
 use pocketmine\command\{CommandSender, Command};
 
-use UIAPI\{from, ModalForm, SimpleForm, CustomForm);
+use UIAPI\{Form, ModalForm, SimpleForm, CustomForm};
 
-class Main extends PluginBase implements Listener {
-	
-  public function onEnabled(){
-    $this->getServer()->getPluginMenager()->registerEvents($this, $this);
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    
   }
   
-  public function onJoin(PlayerJoinEvent $event){
-    $player = event->getPlayer();
-    $this->onMenu($player)
+  public function onJoin(PlayerEventJoin $event){
+    $player = $event->getPlayer();
+    $this->onMenu($player);
+    
   }
   
-  public function onCommand(CommandSender $sender, Command $cmd,  $label, Array $args): bool{
-  	
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args): bool{
     switch($cmd->getName()){
-       case "foxy":{
-       	$this->onMenu($sender);
-       }
-       return true;
+      case "fox":{
+        $this->onMenu($sender);
+        
       }
-   }
-   
-   public function onMenu($sender){
-   	$api = $this->getServer()->getPluginMenager()->getPlugin("FormAPI");
-       $form = $api->createSimpleForm (function (player $seder, int $data = null){
-       	$result = data;
-            if(result === null){
-               return true;
-            }
-            switch($result);
-                 case 0;
-                 $sender->sendMessage("Hello Bro");
-                 break;
-             }
-       });
-       $form->getTitle("Foxy UI");
-       $form->setContenet("Welcome To Server.");
-       $form->addButton("SendMessage", 0, "texture/items/diamond_sword");
-       $form->sendToPlayer($sender);
-           return $from;
-   }
-   
-}           	
+      return true;
+    }
+  }
+  
+  public function onMenu($sender){
+    $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+    $form = $api->createSimpleForm (function (Player $sender, int $data = null){
+      $result = $data;
+      if($result === null){
+        return true;
+      }
+      switch($result){
+        case 0;
+        $sender->sendMessage("Enjoy To Plays");
+        break;
+      }
+    });
+    $form->setTitle("FoxyUI");
+    $form->setContent("Welcome In This Server.");
+    $form->addButton("SendMessage", 0, "texture/items/sword_diamond");
+    $form->sendToPlayer($sender);
+     return $form;
+  }
+  
+}
